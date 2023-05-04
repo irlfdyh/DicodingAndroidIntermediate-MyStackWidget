390439a2b6569852d1cba1854f77be0bcaad47fc
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-04 19:56:03 +0700

    implement all step to create stack widget

---
+++ app/src/main/java/com/dicoding/intermediate/ui/mystackwidget/StackWidgetService.kt
@@ -0,0 +1,11 @@
+package com.dicoding.intermediate.ui.mystackwidget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService : RemoteViewsService() {
+
+    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+
+}
